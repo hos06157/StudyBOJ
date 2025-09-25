abb23b31ee43786c88678d4b6c298381e82223e5
hos06157 <hos06157@naver.com>
2025-09-25 16:11:42 +0900

    feat: 10773

--- 10773.c
+++ 10773.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
-int main() {
+int stack[100000];
+int top = -1;
 
+void push(int value) {
+    stack[++top] = value;
+}
+void pop() {
+    if (top != -1) top--;
+}
+void sum() {
+    int total = 0;
+    for (int i=0; i<=top; i++) total += stack[i];
+    printf("%d", total);
+}
+int main() {
+    int K;
+    scanf("%d", &K);
+    for (int i=0; i<K; i++){
+        int num;
+        scanf("%d", &num);
+        if(num == 0) pop();
+        else push(num);
+    }
+    sum();
+    return 0;
 }
